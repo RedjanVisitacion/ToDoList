69a64f4694a260b35d28265f7bafa3e22c8da1fd
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-19 20:19:49 +0500

    UPDATED

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:practice_app/utils/list.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controler = TextEditingController();
+
+  List todoList = [
+    ['Created by: Redjan Phil S. Visitacion', false],
+  ];
+
+  void checkBoxChanged(int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    setState(() {
+      todoList.add([_controler.text, false]);
+      _controler.clear();
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Simple Todo List',
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return TodoList(
+              taskName: todoList[index][0],
+              taskCompleted: todoList[index][1],
+              onChanged: (value) => checkBoxChanged(index),
+              deleteFunction: (context) => deleteTask(index),
+            );
+          }),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  controller: _controler,
+                  decoration: InputDecoration(
+                    hintText: 'Add a new todo items',
+                    filled: true,
+                    fillColor: Colors.deepPurple.shade200,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
